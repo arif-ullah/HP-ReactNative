8f8a8f8c0cc9c9d6041498f4ff32f0d3249a955d
Arif Ullah <Arif.Ullahmorrisonsplc.co.uk>
2019-07-14 23:29:23 +0100

    Add styles to components

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
 import React, { Fragment } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import HeaderStyle from './HeaderStyle';
+const restaurants = [
+  { name: 'React Cafe', address: '123 Anywhere St' },
+  { name: 'Fancy Restaurant', address: '79 Food Place' },
+  { name: 'Greggs Luxury Food', address: 'Every High St' }
+];
 
 const App = () => {
   return (
     <View>
-      <Text style={{ padding: 40, fontSize: 30, textAlign: 'center', color: '#0066cc', fontWeight: '300' }}>
-        Restaurant Review
-      </Text>
-      <Text>React Cafe</Text>
-      <Text style={{ color: 'gray' }}>123 Anywhere St</Text>
+      <Text style={HeaderStyle.header}>Restaurant Review</Text>
+      {restaurants.map((place, i) => (
+        <View key={i} style={[styles.row, { backgroundColor: i % 2 === 0 ? 'white' : '#eeeeee' }]}>
+          <View style={styles.edges}>
+            <Text>{i + 1}</Text>
+          </View>
+          <View style={styles.nameAddress}>
+            <Text>{place.name}</Text>
+            <Text style={styles.address}>{place.address}</Text>
+          </View>
+          <View style={styles.edges}>
+            <Text>Info</Text>
+          </View>
+        </View>
+      ))}
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  row: { flexDirection: 'row', padding: 5 },
+  edges: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  nameAddress: { flexDirection: 'column', flex: 8 },
+  address: { color: 'gray' }
+});
