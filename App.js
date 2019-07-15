142ba8a5f9f8d8aee7314629c8d7150e2dba268b
Arif Ullah <Arif.Ullahmorrisonsplc.co.uk>
2019-07-15 22:43:01 +0100

    add text input with filter search

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import HeaderStyle from './HeaderStyle';
 const restaurants = [
   { name: 'React Cafe', address: '123 Anywhere St' },
@@ -7,27 +7,44 @@ const restaurants = [
   { name: 'Greggs Luxury Food', address: 'Every High St' }
 ];
 
-const App = () => {
-  return (
-    <View>
-      <Text style={HeaderStyle.header}>Restaurant Review</Text>
-      {restaurants.map((place, i) => (
-        <View key={i} style={[styles.row, { backgroundColor: i % 2 === 0 ? 'white' : '#eeeeee' }]}>
-          <View style={styles.edges}>
-            <Text>{i + 1}</Text>
-          </View>
-          <View style={styles.nameAddress}>
-            <Text>{place.name}</Text>
-            <Text style={styles.address}>{place.address}</Text>
-          </View>
-          <View style={styles.edges}>
-            <Text>Info</Text>
-          </View>
-        </View>
-      ))}
-    </View>
-  );
-};
+class App extends React.Component {
+  state = {
+    search: ''
+  };
+
+  render() {
+    console.log(`search`, this.state.search);
+    return (
+      <View>
+        <Text style={HeaderStyle.header}>Restaurant Reviewer</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Live Search"
+          onChangeText={text => {
+            this.setState({ search: text });
+          }}
+          value={this.state.search}
+        />
+        {restaurants
+          .filter(place => !this.state.search || place.name.toLowerCase().includes(this.state.search))
+          .map((place, i) => (
+            <View key={i} style={[styles.row, { backgroundColor: i % 2 === 0 ? 'white' : '#eeeeee' }]}>
+              <View style={styles.edges}>
+                <Text>{i + 1}</Text>
+              </View>
+              <View style={styles.nameAddress}>
+                <Text>{place.name}</Text>
+                <Text style={styles.address}>{place.address}</Text>
+              </View>
+              <View style={styles.edges}>
+                <Text>Info</Text>
+              </View>
+            </View>
+          ))}
+      </View>
+    );
+  }
+}
 
 export default App;
 
@@ -35,5 +52,15 @@ const styles = StyleSheet.create({
   row: { flexDirection: 'row', padding: 5 },
   edges: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   nameAddress: { flexDirection: 'column', flex: 8 },
-  address: { color: 'gray' }
+  address: { color: 'gray' },
+  input: {
+    marginBottom: 30,
+    padding: 10,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    borderBottomWidth: 1,
+    borderColor: '#444',
+    backgroundColor: '#eee'
+  }
 });
